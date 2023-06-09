b4b7322a4706c033ee75d2eb63b6e5276a92dd59
ibnul.imtiaz <imtiazeemel42@gmail.com>
2023-06-09 16:15:56 +0600

    TopBar

---
+++ app/src/main/java/com/imtiaz/jetpack/MainActivity.kt
@@ -0,0 +1,88 @@
+package com.imtiaz.jetpack
+
+import android.annotation.SuppressLint
+import android.content.res.Configuration
+import android.graphics.fonts.FontStyle
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.imtiaz.jetpack.ui.theme.JetPackTheme
+
+class MainActivity : ComponentActivity() {
+
+    @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetPackTheme {
+                Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            title = {
+                                Text("JetPack")
+                            },
+                            navigationIcon = {
+                                IconButton(onClick = {  }) {
+                                    Icon(Icons.Filled.Menu, contentDescription = "menu" )
+                                    
+                                }
+                            },
+                            actions = {
+                                IconButton(onClick = {  }) {
+                                    Icon(Icons.Filled.Notifications, contentDescription = "notifications")
+                                }
+                                IconButton(onClick = {  }) {
+                                    Icon(Icons.Filled.Search, contentDescription = "search")
+                                }
+                            }
+                        )
+                    },
+                    floatingActionButton = {
+                        FloatingActionButton(onClick = {}) {
+                            IconButton(onClick = {  }) {
+                                Icon(Icons.Filled.Add, contentDescription = "add")
+                            }
+                        }
+                    },
+                    floatingActionButtonPosition = FabPosition.End
+                ) {
+                    var value = "Android"
+                    Greeting(name = "World")
+                }
+
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(
+        text = "Hello $name!",
+        color = MaterialTheme.colors.secondary
+
+
+
+    )
+}
+
+@Preview(showBackground = true, name = "light mode")
+@Preview(showBackground = true, name = "dark mode", uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun DefaultPreview() {
+    JetPackTheme {
+        Greeting("Android")
+    }
+}
